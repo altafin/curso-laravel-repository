35286f916cabc3f52c084aea851acc1fe0269363
marcelo <altafin@uel.br>
2023-05-04 11:53:30 -0300

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin')
+
+@section('content_header')
+    <h1>dashboard</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+@stop
+
+
+{{--@extends('layouts.app')--}}
+
+{{--@section('content')--}}
+{{--<div class="container">--}}
+{{--    <div class="row justify-content-center">--}}
+{{--        <div class="col-md-8">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-header">{{ __('Dashboard') }}</div>--}}
+
+{{--                <div class="card-body">--}}
+{{--                    @if (session('status'))--}}
+{{--                        <div class="alert alert-success" role="alert">--}}
+{{--                            {{ session('status') }}--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
+
+{{--                    {{ __('You are logged in!') }}--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--@endsection--}}
